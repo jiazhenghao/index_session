5a75bc9e880ec69c4a2815ad6309c582f9b5f630
Zhenghao Jia <cherie-xia@hotmail.com>
2017-07-01 17:01:24 +0800

    upload
    Ver 0.0.1

---
+++ login_session.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$("#login").click(function(e){
+		e.preventDefault();
+		$.ajax({
+			type:"post",
+			url:"/login_session/",
+			data:{"username":$("#username").val(),
+					"password":$("#password").val(),
+			},
+			success:function(callback){
+				console.log(callback);
+				callback=jQuery.parseJSON(callback);
+				if(callback.status==1){
+					alert("登陆成功");
+					//$.cookie("username", $("#username").val(), {path: '/' })
+					window.location.href = "/hello_session/"
+				}
+				else{
+					if(callback.error_code==1){
+						alert("密码错误！");
+					}
+					else if(callback.error_code==2){
+						alert("该用户尚未注册！");
+					}
+					
+				}
+				
+			},
+			async:true
+		});
+	});
+	
+	
+});
+
+
